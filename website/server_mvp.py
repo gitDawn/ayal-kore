632693ce31337c57298c13f49f5c851e4492c444
gitDawn <shahar8@gmail.com>
2026-01-31 18:23:00 +0200

    Add dotenv load for environment variables

--- website/server_mvp.py
+++ website/server_mvp.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
